ed7535aa5f42ad2b57235e2d54a31ee05b25f055
Ademola Al-ameen <ademolaalameen86@gmail.com>
2026-07-08 01:02:41 +0100

    db 1.2

--- src/db.ts
+++ src/db.ts
@@ -1,2 +1,6 @@
-const { PrismaClient } = require("@prisma/client");
+import pkg from "@prisma/client";
+
+// Support environments where @prisma/client may not export named bindings
+const { PrismaClient } = (pkg as any);
+
 export const prisma = new PrismaClient();
